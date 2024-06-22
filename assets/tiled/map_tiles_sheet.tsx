9fec63bcec8e68a60cd45ff1c224d9cfccea47b1
Nils Nahooy <nils.nahooy@live.com>
2024-06-22 11:16:41 +0200

    Add assets from original project

---
+++ assets/tiled/map_tiles_sheet.tsx
@@ -0,0 +1,83 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="map_tiles_sheet" tilewidth="32" tileheight="32" tilecount="100" columns="10">
+ <image source="map_tiles_sheet.png" width="320" height="320"/>
+ <tile id="24">
+  <animation>
+   <frame tileid="20" duration="250"/>
+   <frame tileid="21" duration="250"/>
+   <frame tileid="22" duration="250"/>
+   <frame tileid="23" duration="250"/>
+  </animation>
+ </tile>
+ <tile id="30">
+  <objectgroup draworder="index" id="3">
+   <object id="15" type="TileCollision" x="2.4375" y="0">
+    <polygon points="0,0 0,2.875 1.75,4.625 0.9375,6.1875 0.0625,7.3125 -0.0625,14.75 1.625,16.25 0.3125,17.4375 -0.0625,19.125 0.125,23.75 2.125,26.3125 2.875,27.875 6.125,28 8.25,29.1875 11.6875,29.5 13.625,28.5 14.1875,27.8125 15.4375,29.125 16.4375,29.8125 20,29.5 23.0625,26.875 24.875,28.5625 25.8125,29.625 29.5,29.5625 29.4375,31.0625 -1.9375,30.3125 -1.75,0"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="31">
+  <objectgroup draworder="index" id="2">
+   <object id="1" type="TileCollision" x="0" y="30" width="32" height="2"/>
+  </objectgroup>
+ </tile>
+ <tile id="32">
+  <objectgroup draworder="index" id="2">
+   <object id="1" type="TileCollision" x="0" y="30" width="32" height="2"/>
+  </objectgroup>
+ </tile>
+ <tile id="33">
+  <objectgroup draworder="index" id="2">
+   <object id="1" type="TileCollision" x="0" y="30" width="32" height="2"/>
+  </objectgroup>
+ </tile>
+ <tile id="34">
+  <objectgroup draworder="index" id="2">
+   <object id="1" type="TileCollision" x="0" y="28.3636">
+    <polygon points="0,0 1.72727,0.0909091 2,0.727273 11.6364,0.727273 11.8182,1.63636 15.9091,1.72727 16.1818,0.727273 23.4545,0.727273 23.8182,1.90909 26.3636,1.81818 29.2727,-0.181818 29.2727,-4 30.3636,-4.45455 30.2727,-9.90909 28.2727,-10.7273 28.0909,-12 30.0909,-13.2727 30.2727,-20.4545 29.4545,-21.0909 29.3636,-21.7273 30.2727,-22.6364 30.0909,-28.2727 31.7273,-28.1818 31.6364,2.81818 29.0909,3.09091 8.18182,3.18182 0.181818,3.18182"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="35">
+  <objectgroup draworder="index" id="2">
+   <object id="1" type="TileCollision" x="0" y="0" width="32" height="2"/>
+   <object id="2" type="TileCollision" x="0" y="2" width="2" height="30"/>
+  </objectgroup>
+ </tile>
+ <tile id="36">
+  <objectgroup draworder="index" id="2">
+   <object id="1" type="TileCollision" x="0" y="0" width="32" height="2"/>
+  </objectgroup>
+ </tile>
+ <tile id="37">
+  <objectgroup draworder="index" id="2">
+   <object id="1" type="TileCollision" x="0" y="0" width="32" height="2"/>
+  </objectgroup>
+ </tile>
+ <tile id="38">
+  <objectgroup draworder="index" id="2">
+   <object id="1" type="TileCollision" x="0" y="0" width="32" height="2"/>
+  </objectgroup>
+ </tile>
+ <tile id="39">
+  <objectgroup draworder="index" id="2">
+   <object id="1" type="TileCollision" x="0" y="0" width="32" height="2"/>
+   <object id="2" type="TileCollision" x="30" y="2" width="2" height="30"/>
+  </objectgroup>
+ </tile>
+ <tile id="44">
+  <objectgroup draworder="index" id="2">
+   <object id="1" type="TileCollision" x="0" y="0" width="2" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="45">
+  <objectgroup draworder="index" id="2">
+   <object id="1" type="TileCollision" x="0" y="0" width="2" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="49">
+  <objectgroup draworder="index" id="2">
+   <object id="1" type="TileCollision" x="30" y="0" width="2" height="32"/>
+  </objectgroup>
+ </tile>
+</tileset>
---
+++ assets/tiled/objects.tsx
@@ -0,0 +1,15 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="objects" tilewidth="128" tileheight="128" tilecount="1" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="4">
+  <properties>
+   <property name="Ysorting" type="bool" value="true"/>
+  </properties>
+  <image width="128" height="128" source="objects/Ash-10.png"/>
+  <objectgroup draworder="index" id="3">
+   <object id="2" type="ObjectCollision" x="57.6364" y="128">
+    <polygon points="0,0 5.81818,-4.54545 5.63636,-8.72727 13.2727,-8.72727 13.6364,-5.27273 19.8182,0.181818"/>
+   </object>
+  </objectgroup>
+ </tile>
+</tileset>
