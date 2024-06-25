852869e778af2ad69cc82271d580aa1826c8904a
Nils Nahooy <nils.nahooy@live.com>
2024-06-25 20:18:45 +0200

    more looking into porting manager and entity

--- src/ecs/entity.rs
+++ src/ecs/entity.rs
@@ -2,4 +2,5 @@ pub struct Entity {}
 
 impl Entity {
     pub fn update(&mut self) {}
+    pub fn draw(&self) {}
 }
--- src/ecs/manager.rs
+++ src/ecs/manager.rs
@@ -1,21 +1,33 @@
 use super::entity::Entity;
 
+const MAXGROUPS: usize = 32;
+
 pub struct Manager<'a> {
-    entities: Vec<Box<Entity>>,
+    entities: Vec<Entity>,
     grouped_entities: Vec<Vec<&'a Entity>>,
 }
 
 impl<'a> Manager<'_> {
-    fn new() -> Manager<'a> {
+    pub fn new() -> Manager<'a> {
         Manager {
             entities: Vec::new(),
-            grouped_entities: Vec::with_capacity(32),
+            grouped_entities: Vec::with_capacity(MAXGROUPS),
         }
     }
 
-    fn update(&mut self) {
+    pub fn update(&mut self) {
         for e in &mut self.entities {
             e.update();
         }
+    }
+
+    pub fn draw(&self) {
+        for e in &self.entities {
+            e.draw();
+        }
+    }
+
+    pub fn refresh(&mut self) {
+        for entity_group in &mut self.grouped_entities {}
     }
 }
